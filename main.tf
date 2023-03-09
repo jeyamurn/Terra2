cb0f026c2d0fd6a024a09c7462dcc5a14d7c03a9
jeyamurn <36784295+jeyamurn@users.noreply.github.com>
2023-03-09 19:19:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+//terraform {
+  //required_providers {
+   // aws = {
+    //  source  = "hashicorp/aws"
+    //  version = "~> 4.16"
+  //  }
+ // }
+
+ // required_version = ">= 1.2.0"
+//}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "TekvChe" {
+  ami           = "ami-0948bc30d008b0e5a"
+  instance_type = "m5n.large"
+  
+  vpc_security_group_ids = ["sg-05f9dbdf02a29fb32"]
+  subnet_id = "subnet-0ee2b881b15cff1da"
+   
+  tags = {
+    Name = "TekvChe"
+  }
+}
